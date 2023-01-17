139c997f9300b8bc915b73dd033c7b5dbdca72e7
Marin039 <115377867+Marin039@users.noreply.github.com>
2023-01-18 02:44:08 +0300

    Add files via upload

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,48 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user1 = new User("Bob", "Smith", "bobsmith@gmail.com");
+      User user2 = new User("Ann", "Jones", "annjones@gmail.com");
+
+      Car car1 = new Car("Audi", 123);
+      Car car2 = new Car ("Mercedes", 456);
+
+      user1.setUserCar(car1);
+      user2.setUserCar(car2);
+      userService.add(user1);
+      userService.add(user2);
+
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println();
+      }
+      System.out.println(userService.getUserByCar("Audi", 123));
+
+      context.close();
+   }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,55 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table (name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private Long id;
+    @Column (name = "model")
+    private String model;
+    @Column (name = "series")
+    private int series;
+
+
+    public Car() {}
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    @Override
+    public String toString() {
+        return "model='" + model + '\'' +
+                ", series=" + series;
+    }
+}
